7ac85eefcdfff0106fe442cc42f2ca55ffb00be8
Charles Wooley <charleswooley@Charless-MBP.home>
2019-04-30 15:05:49 -0400

    fixed bug

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ export default class OpengraphReactComponent extends Component {
           <iframe
             width="560"
             height="315"
-            src={value.secure_url}
+            src={resultsToUse.video}
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
